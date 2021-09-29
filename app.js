2048fb2a8a7b40b5d5730a8e622a2a9632666c34
Nitatemic <alexandre@nitatemic.ovh>
2021-09-29 22:30:57 +0200

    DB connected !

--- app.js
+++ app.js
@@ -1,13 +1,66 @@
-const express = require("express");
+const express = require("express"); //ExpressJS module
+const mongoose = require("mongoose");
+
+require('dotenv').config();
+const db = process.env.MONGO_URI;  //Variable pour l'URL de la BDD
+
+mongoose.connect(db,
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('DB Connected'))
+  .catch(err => console.log(err));
+
+const argon2 = require('argon2'); //Argon2 module (For password hashing)
+const User = require('./models/User');
 
 const app = express();
 
-app.get("/", function (req, res) {
-  res.send("Hello World!");
+//Ajouter une route POST pour créer un nouvel utilisateur et renvoyer un message de confirmation
+//Données récupérés : mail et password.
+//Hachage du mot de passe de l'utilisateur, ajout de l'utilisateur à la base de données
+app.post("/api/auth/signup", function (req, res) {
+  //Parser le body de la requête
+  const {mail, password} = req.body;
+  //Vérifier que les champs sont remplis
+  if (!mail || !password) {
+    res.status(400).json({
+      error: "Missing mail or password"
+    });
+    return;
+  }
+  //Vérifier que le mail n'est pas déjà utilisé
+  User.findOne({
+    mail: mail
+  }, function (err, user) {
+    if (err) {
+      res.status(500).json({
+        error: "Internal error"
+      });
+      return;
+    }
+    if (user) {
+      res.status(400).json({
+        error: "Mail already used"
+      });
+      return;
+    }
+
+    //Hachage du mot de passe
+    argon2.hash(password).then(hash => {
+      //Création d'un nouvel utilisateur
+      const user = new User({
+        mail,
+        password: hash
+      });
+      //Sauvegarde de l'utilisateur dans la base de données
+      user.save().then(() => {
+        res.send("Utilisateur créé");
+      }).catch(err => {
+        res.status(500).send(err);
+      });
+    });
+  });
 });
 
-app.post("/post", function (req, res) {
-  res.send("Got a POST request");
-});
 
 module.exports = app;
