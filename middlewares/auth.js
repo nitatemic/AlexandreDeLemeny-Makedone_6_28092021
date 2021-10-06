8bc3303660183500695e45cd46679bf0a3fea0c4
Nitatemic <alex@nitatemic.ovh>
2021-10-06 23:33:49 +0200

    Middleware checkToken

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,5 +1,8 @@
 const express = require("express"); //ExpressJS module
 const app = express();
+require('dotenv').config();
+const SECRET = process.env.SECRET_KEY;
+const jwt = require('jsonwebtoken');
 app.use(express.urlencoded({extended: true}));
 app.use(express.json()) // To parse the incoming requests with JSON payloads
 
@@ -13,3 +16,22 @@ exports.checkMail = (req, res, next) => {
     }
     next();
 }
+
+
+
+exports.checkToken = (req, res, next) => {
+    try {
+        const token = req.headers.authorization.split(' ')[1];
+        const decodedToken = jwt.verify(token, SECRET);
+        const userId = decodedToken.userId;
+        if (req.body.userId && req.body.userId !== userId) {
+            throw 'Invalid user ID';
+        } else {
+            next();
+        }
+    } catch {
+        res.status(401).json({
+            error: new Error('Invalid request!')
+        });
+    }
+};
