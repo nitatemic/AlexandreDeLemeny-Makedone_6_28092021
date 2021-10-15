9a7c824fc200d4ac2c38cf574c80fd13f081d94c
Nitatemic <alex@nitatemic.ovh>
2021-10-15 17:53:05 +0200

    IsAlive function

--- controllers/isAlive.js
+++ controllers/isAlive.js
@@ -3,4 +3,4 @@ exports.isAlive = function(req, res) {
     res.status(200).send("I'm a-stayin' alive, stayin' alive\n" +
         "Ah, ah, ah, ah, stayin' alive, stayin' alive\n" +
         "Ah, ah, ah, ah, stayin' ali-i-i-i-ive");
-};
+    };
